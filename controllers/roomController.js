7df88d0949b42aa2bbbb31b334092b67d12559e2
Jeremy Marotta <firefreet@gmail.com>
2020-04-23 18:03:22 -0400

    save answer adds user if doesnt yet exist

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -17,26 +17,37 @@ RoomController = {
 
   saveAnswer: (req, res) => {
     const answerData = req.body;
-    console.log(answerData);
     const { roomId, userName, answer, roundNumber, questionNumber } = answerData;
     // check the room for an answer to that question/round by that user
-    RoomModel.find({ '_id': ObjectId(roomId), 'participants.name': userName, 'participants.responses.roundNumber': roundNumber, 'participants.responses.questionNumber': questionNumber  }).then((data)=>{
-      // if it is not found
-      if(data.length === 0) {
-        // add their answer in
-        RoomModel.updateOne({ '_id': ObjectId(roomId), 'participants.name': userName }, { $push: { 'participants.$.responses': { 'answer': answer, 'questionNumber': questionNumber, 'roundNumber': roundNumber } } })
-        .then(update => {
+    RoomModel.findOne({ '_id': ObjectId(roomId) }).then(async (data) => {
+      try {
+        var update = false;
+        // get index of the participant in the array
+        const index = data.participants.findIndex(element => {
+          return element.name === userName
+        })
+        // if user is not yet listed in the room, add them
+        if (index === -1) {
+          await RoomModel.updateOne({ '_id': ObjectId(roomId) }, { $push: { participants: { name: userName, responses: [] } } })
+          update = await RoomModel.updateOne({ '_id': ObjectId(roomId), 'participants.name': userName }, { $push: { 'participants.$.responses': { 'answer': answer, 'questionNumber': questionNumber, 'roundNumber': roundNumber } } })
+
+        } else {
+          // check through their responses to see if this question and round already has an answer
+          const prevAnswer = data.participants[index].responses.filter(element => {
+            if (element.roundNumber === roundNumber && element.questionNumber === questionNumber) return true
+          });
+          // if it was not found
+          if (prevAnswer.length === 0) {
+            // add their answer in
+            update = await RoomModel.updateOne({ '_id': ObjectId(roomId), 'participants.name': userName }, { $push: { 'participants.$.responses': { 'answer': answer, 'questionNumber': questionNumber, 'roundNumber': roundNumber } } })
+          }
           console.log(update);
           res.json(update);
-        })
-        .catch(err => {
-          console.log(err);
-          res.json(err);
-        })  
+        }
+      } catch (err) {
+        console.log(err);
+        res.json(err);
       }
-    }).catch(err => {
-      console.log(err);
-      res.json(err);
     })
   }
 }
