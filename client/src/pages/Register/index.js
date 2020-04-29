cb1f336ecdfb110e5304feff79a5686aa2c2eca5
Nathan Sartain <nathanmsartain@gmail.com>
2020-04-28 20:36:24 -0400

    Added user friendly /register data validation.

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -5,10 +5,15 @@ import cookies from '../../utils/cookie';
 
 function Register(props) {
   const [firstName, setfirstName] = useState('');
+  const [validFirstName, setValidFirstName] = useState(true);
   const [lastName, setlastName] = useState('');
+  const [validLastName, setValidLastName] = useState(true);
   const [email, setEmail] = useState('');
+  const [validEmail, setValidEmail] = useState(true);
   const [password, setPassword] = useState('');
+  const [validPassword, setValidPassword] = useState(true);
   const [validation, setValidation] = useState('');
+  const [finishedForm, setFinishedForm] = useState(true);
   const [user, setUser] = useState('');
 
   const firstNameRef = useRef();
@@ -16,10 +21,28 @@ function Register(props) {
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  useEffect(() => {
-    console.log(user);
+  const invalidFirstName = <p className="text-danger">First name can only contain letters.</p>
+  const invalidLastName = <p className="text-danger">Last name can only contain letters.</p>
+  const invalidEmail = <p className="text-danger">Please enter a valid email address.</p>
+  const invalidPassword = <p className="text-danger">Passwords must be at least 8 digits long.</p>
+  const incompleteForm = <p className="text-danger">Please fill out the form completely.</p>
 
-  }, [user]);
+  const invalidNameChars = ' `~!@#$%^&*()-=,./?\\|[]{}_+;\':"<>?0123456789';
+
+  useEffect(() => {
+    setFinishedForm(true);
+    if (firstName)
+      validateFirstName();
+
+    if (lastName)
+      validateLastName();
+
+    if (email)
+      validateEmail();
+
+    if (password)
+      validatePassword();
+  }, [firstName, lastName, email, password]);
 
   function handleInput(e) {
     switch (e.target.id) {
@@ -40,8 +63,58 @@ function Register(props) {
     }
   }
 
+  const validateFirstName = () => {
+    let valid = true;
+
+    for (let i = 0; i < firstName.length; i++) {
+      if (invalidNameChars.includes(firstName.charAt(i))) {
+        valid = false;
+        setValidFirstName(false);
+      }
+    }
+
+    if (valid && !validFirstName)
+      setValidFirstName(true);
+  }
+
+  const validateLastName = () => {
+    let valid = true;
+
+    for (let i = 0; i < lastName.length; i++) {
+      if (invalidNameChars.includes(lastName.charAt(i))) {
+        valid = false;
+        setValidLastName(false);
+      }
+    }
+
+    if (valid && !validLastName)
+      setValidLastName(true);
+  }
+
+  const validateEmail = () => {
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      setValidEmail(false);
+    } else {
+      setValidEmail(true);
+    }
+  }
+
+  const validatePassword = () => {
+    if (password.length < 8) {
+      setValidPassword(false);
+    } else {
+      setValidPassword(true);
+    }
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!firstName || !lastName || !email || !password
+      || !validFirstName || !validLastName || !validEmail || !validPassword) {
+    setFinishedForm(false);
+    return;
+  }
 
     let userRegData = {
       firstName: firstName,
@@ -88,6 +161,7 @@ function Register(props) {
                   id="fname"
                   aria-describedby="firstName"
                   placeholder="Enter First Name" />
+                  {!validFirstName ? invalidFirstName : null}
               </div>
               <div className="form-group col-md-6">
                 <label htmlFor="lastName">Last Name</label>
@@ -99,6 +173,7 @@ function Register(props) {
                   id="lname"
                   aria-describedby="lastName"
                   placeholder="Enter Last Name" />
+                  {!validLastName ? invalidLastName : null}
               </div>
             </div>
             <div className="form-group">
@@ -111,6 +186,7 @@ function Register(props) {
                 id="email"
                 aria-describedby="email"
                 placeholder="Enter Email" />
+                {!validEmail ? invalidEmail : null}
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
@@ -122,6 +198,7 @@ function Register(props) {
                 id="password"
                 aria-describedby="password"
                 placeholder="Enter Password" />
+                {!validPassword ? invalidPassword : null}
             </div>
             <button
               onClick={handleSubmit}
@@ -129,6 +206,7 @@ function Register(props) {
               className="btn btn-warning"
             >
               Submit</button>
+              {!finishedForm ? incompleteForm : null}
           </form>
           {validation !== '' ?
             <div className="alert alert-danger alert-dismissible fade show mt-3" role="alert">
