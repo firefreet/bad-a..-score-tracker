9eb40995c54829499d0d69f06532e37259ab7af8
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-05-04 16:24:10 -0400

    Styled WelcomeBackModal

--- client/src/components/WelcomeBackModal/index.js
+++ client/src/components/WelcomeBackModal/index.js
@@ -2,6 +2,7 @@ import {Modal, Button} from 'react-bootstrap';
 import React, { useContext } from 'react';
 import RoomContext from '../../utils/RoomContext';
 import API from '../../utils/API';
+import './style.scss';
 
 function WelcomeBackModal(props) {
   const { roomState: { loggedIn }, roomState, setRoomState } = useContext(RoomContext);
@@ -21,16 +22,16 @@ function WelcomeBackModal(props) {
   return (
     <Modal show={props.show} onHide={props.handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Welcome Back!</Modal.Title>
+        <Modal.Title><img src="/img/info.svg" className="modalInfoSvg align-bottom" />Welcome Back!</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        <p>Hello again, {props.participant}. Would you like to resume your session in room {props.roomCode}?</p>
+        <p>Hello again, <strong className="responseIoLink">{props.participant}</strong>. Would you like to resume your session in room <strong className="responseIoLink">{props.roomCode}</strong>?</p>
       </Modal.Body>
 
       <Modal.Footer>
-        <Button variant="secondary" onClick={props.handleClose}>No</Button>
-        <Button variant="primary" onClick={resumeSession}>Yes</Button>
+        <Button variant="outline-danger" size="sm" onClick={props.handleClose}>No</Button>
+        <Button variant="warning" size="sm" onClick={resumeSession}>Yes</Button>
       </Modal.Footer>
     </Modal>
   )
